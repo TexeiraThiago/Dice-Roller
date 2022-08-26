5b91dca7a38a16832f5601e5b530a00c6a0fcda9
TexeiraThiago <thiagot@id.uff.br>
2022-08-26 18:49:00 -0300

    inclue comments

--- app/src/main/java/br/com/firstaplication/dice/MainActivity.kt
+++ app/src/main/java/br/com/firstaplication/dice/MainActivity.kt
@@ -10,17 +10,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val rollButton : Button = findViewById(R.id.btnRoll)
+        val rollButton: Button = findViewById(R.id.btnRoll)
 
-        rollButton.setOnClickListener {
-            rollDice()
-        }
+        rollButton.setOnClickListener { rollDice() }
     }
-
+    /**
+     * Roll the dice and update the screen with the result
+     */
     private fun rollDice() {
+        // Create a new object with 6 sides and roll it
         val dice = Dice(6)
         val diceRoll = dice.roll()
-        val resultTextView : TextView = findViewById(R.id.etText)
-        resultTextView.text =  diceRoll.toString()
+        // Update screen with the dice roll
+        val resultTextView: TextView = findViewById(R.id.etText)
+        resultTextView.text = diceRoll.toString()
     }
 }
