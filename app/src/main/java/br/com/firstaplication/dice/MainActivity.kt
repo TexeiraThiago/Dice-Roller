22ee3f30b89368587d689654bbd2fcbe2915abd9
TexeiraThiago <thiagot@id.uff.br>
2022-08-26 19:28:11 -0300

    set imagens onclickListener

--- app/src/main/java/br/com/firstaplication/dice/MainActivity.kt
+++ app/src/main/java/br/com/firstaplication/dice/MainActivity.kt
@@ -2,7 +2,7 @@ package br.com.firstaplication.dice
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +13,9 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.btnRoll)
 
         rollButton.setOnClickListener { rollDice() }
+
+
+        rollDice();
     }
     /**
      * Roll the dice and update the screen with the result
@@ -22,7 +25,17 @@ class MainActivity : AppCompatActivity() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
         // Update screen with the dice roll
-        val resultTextView: TextView = findViewById(R.id.etText)
-        resultTextView.text = diceRoll.toString()
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        val drawarbleResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawarbleResource)
+        diceImage.contentDescription = diceRoll.toString()
+
     }
 }
